cc0e001c90781357d02be13e86377b94f2220add
toutou <toutounode@gmail.com>
2022-05-26 19:55:46 +0900

    コメントを追加

--- OSCSend.go
+++ OSCSend.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/hypebeast/go-osc/osc"
@@ -9,16 +8,19 @@ import (
 
 func main() {
 	client := osc.NewClient("127.0.0.1", 9000)
-	msgMin := osc.NewMessage("/avatar/parameters/LeftMin")
-	msgHour := osc.NewMessage("/avatar/parameters/RightHour")
+
 	for {
+		msgMin := osc.NewMessage("/avatar/parameters/LeftMin")
+		msgHour := osc.NewMessage("/avatar/parameters/RightHour")
 		var t = time.Now()
 		hour := t.Hour()
 		minute := t.Minute()
-		fmt.Print(t.Local().Hour(), t.Local().Minute(), "\n")
+		// fmt.Print(t.Local().Hour(), t.Local().Minute(), "\n")
+		// 分を60分に対応する[0,1]のfloatに変換
 		fminute := float32(minute) / float32(60)
+		// 時間を12時間に対応する[0,1]のfloatに変換
 		fhour := float32(int(hour%12))/float32(12) + fminute/float32(12)
-		fmt.Print(fhour, fminute, "\n")
+		// fmt.Print(fhour, fminute, "\n")
 		msgMin.Append(float32(fminute))
 		msgHour.Append(float32(fhour))
 		client.Send(msgHour)
